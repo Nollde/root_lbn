b2ee0436dc05b1b1f51573d05aef2746b834a086
Dennis Noll <github_001@nollde.de>
2020-10-31 20:47:20 +0100

    [notebook] added Normal Layer + BatchNormalization

--- root_to_lbn.py
+++ root_to_lbn.py
@@ -17,7 +17,7 @@
 # All the needed software is pinned in the `environment.yml` file.
 # It can be installed and sourced using [`conda`](https://docs.conda.io/en/latest/).
 
-# In[ ]:
+# In[1]:
 
 
 import itertools
@@ -32,33 +32,25 @@ from sklearn.utils.class_weight import compute_class_weight
 from sklearn.metrics import confusion_matrix
 from matplotlib import pyplot as plt
 
+from tools.keras import Normal
+
 TLorentzVectorArray = uproot_methods.classes.TLorentzVector.TLorentzVectorArray
 
 
-# In[ ]:
+# Check for correct tensorflow version and GPU availability:
+
+# In[2]:
 
 
 assert tf.__version__.startswith("2")
+print("Available GPU:", tf.config.list_physical_devices('GPU'))
 
 
-# ## Function definitions
-# First we will define some useful fuctions for data shuffling, converting root trees into numpy arrays, shaping the numpy arrays the way we want to use them, selecting our events and plotting.
+# ## Data
+# 
+# Define some functions that handle uproot data formats and convert `TLorentzVectors` consisting of the transverse momentum, the pseudorapidity, the phi angle and the mass to four vectors consisting of the energy and the three momentum.
 
-# Data shuffling:
-
-# In[ ]:
-
-
-def shuffle_together(*arrays):
-    first_array = arrays[0]
-    assert [len(first_array) == len(array) for array in arrays[1:]]
-    p = np.random.permutation(len(first_array))
-    return [array[p] for array in arrays]
-
-
-# We need some functions to handle uproot data formats and to convert `TLorentzVectors` consisting of the transverse momentum, the pseudorapidity, the phi angle and the mass to four vectors consisting of the energy and the three momentum.
-
-# In[ ]:
+# In[3]:
 
 
 def tree_to_lorentz(tree, name="Jet"):
@@ -73,33 +65,52 @@ def tree_to_lorentz(tree, name="Jet"):
 def tree_to_array(tree, mask, name="Jet", n=4):
     lorentz = tree_to_lorentz(tree, name=name)
     array = np.array([
-        lorentz[mask].E[:, :n],
-        lorentz[mask].x[:, :n],
-        lorentz[mask].y[:, :n],
-        lorentz[mask].z[:, :n],
+        lorentz[mask].E[:, :n].pad(n).fillna(0).flatten().reshape(-1, n),
+        lorentz[mask].x[:, :n].pad(n).fillna(0).flatten().reshape(-1, n),
+        lorentz[mask].y[:, :n].pad(n).fillna(0).flatten().reshape(-1, n),
+        lorentz[mask].z[:, :n].pad(n).fillna(0).flatten().reshape(-1, n),
     ])
     return np.moveaxis(array, 0, 2)
 
 
+# Load data for signal and background:
+
+# In[4]:
+
+
+DATA_PATH = "/eos/user/d/dnoll/HH/lbn/%s.root"
+DATA_PATH = "/net/scratch/dn801636/projects/root_to_lbn/data/%s.root"
+signal, background = uproot.open(DATA_PATH % "signal"), uproot.open(DATA_PATH % "background")
+signal_tree, background_tree = signal["Events"], background["Events"]
+
+
 # In this simple example we will select events with exactly one electron with transverse momentum greater than 25 GeV and equal or more than four jets:
 
-# In[ ]:
+# In[5]:
 
 
 def event_selection(tree):
     e_mask = tree["nElectron"].array() == 1
-    pt_mask = any(tree["Electron_pt"].array() > 25)
+    pt_mask = (tree["Electron_pt"].array() > 25).any()
     jet_mask = tree["nJet"].array() >=4
     return e_mask * pt_mask * jet_mask
 
 
+# Create a boolean mask for the event selection:
+
+# In[6]:
+
+
+signal_mask, background_mask = event_selection(signal_tree), event_selection(background_tree)
+
+
 # Specification of low and high level input data. As low level inputs, we will use the four momenta of the four jets with the highest momentum, and the electron, as high level feature we will use the variable "MET_pt" which is defined on the event level:
 
-# In[ ]:
+# In[7]:
 
 
 def get_low_level(tree, mask):
-    jets = tree_to_array(tree, mask, name="Jet")
+    jets = tree_to_array(tree, mask, name="Jet", n=4)
     electron = tree_to_array(tree, mask, name="Electron", n=1)
     events = np.concatenate([jets, electron], axis=1)
     return events
@@ -109,56 +120,9 @@ def get_high_level(tree, mask, variables=["MET_pt"]):
     return np.moveaxis(output, 0, 1)
 
 
-# Plotting a simple confusion matrix for the final classificaiton:
-
-# In[ ]:
-
-
-def plot_confusion_matrix(cm, class_names):
-    figure = plt.figure(figsize=(4, 4))
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-    plt.title("Confusion matrix")
-    plt.colorbar()
-    tick_marks = np.arange(len(class_names))
-    plt.xticks(tick_marks, class_names, rotation=45)
-    plt.yticks(tick_marks, class_names)
-
-    cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
-
-    threshold = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        color = "white" if cm[i, j] > threshold else "black"
-        plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.savefig("hh_tt.pdf")
-    plt.show()
-
-
-# ## Data
-# Load data for signal and background:
-
-# In[ ]:
-
-
-DATA_PATH = "/eos/user/d/dnoll/HH/lbn/%s.root"
-signal, background = uproot.open(DATA_PATH % "signal"), uproot.open(DATA_PATH % "background")
-signal_tree, background_tree = signal["Events"], background["Events"]
-
-
-# Create a boolean mask for the event selection:
-
-# In[ ]:
-
-
-signal_mask, background_mask = event_selection(signal_tree), event_selection(background_tree)
-
-
 # Get low and high level data, which will be used for the LBN and the following DNN respectively:
 
-# In[ ]:
+# In[8]:
 
 
 signal_ll, signal_hl = get_low_level(signal_tree, signal_mask), get_high_level(signal_tree, signal_mask)
@@ -167,11 +131,17 @@ background_ll, background_hl = get_low_level(background_tree, background_mask), 
 
 # Encode targets to one_hot vectors:
 
-# In[ ]:
+# In[9]:
 
 
-signal_targets = tf.keras.backend.one_hot(np.ones(signal_ll.shape[0]), 2)
-background_targets = tf.keras.backend.one_hot(np.zeros(background_ll.shape[0]), 2)
+np.ones(signal_ll.shape[0])[..., None] * np.array([1, 0])
+
+
+# In[10]:
+
+
+signal_targets = np.ones(signal_ll.shape[0])[..., None] * np.array([0, 1])
+background_targets = np.ones(background_ll.shape[0])[..., None] * np.array([1, 0])
 
 
 # We have a factor ~10 more background than signal events and have to explicitly care that the network does not only concentrate on the background class.
@@ -180,7 +150,7 @@ background_targets = tf.keras.backend.one_hot(np.zeros(background_ll.shape[0]), 
 # We will use it in this example. Because the differences are already pretty large (factor ~10), our validation accuracy will fluctuate.
 # If the differences are to large, we have to crop the larger class or, as an even better approach, sample up the smaller class.
 
-# In[ ]:
+# In[11]:
 
 
 # n_signal = signal_ll.shape[0]
@@ -189,7 +159,17 @@ background_targets = tf.keras.backend.one_hot(np.zeros(background_ll.shape[0]), 
 
 # Concatenate and shuffle signal and background samples:
 
-# In[ ]:
+# In[12]:
+
+
+def shuffle_together(*arrays):
+    first_array = arrays[0]
+    assert [len(first_array) == len(array) for array in arrays[1:]]
+    p = np.random.permutation(len(first_array))
+    return [array[p] for array in arrays]
+
+
+# In[13]:
 
 
 ll = np.concatenate([signal_ll, background_ll], axis=0).astype(np.float32)
@@ -199,42 +179,45 @@ targets = np.concatenate([signal_targets, background_targets], axis=0)
 ll, hl, targets = shuffle_together(ll, hl, targets)
 
 
-# Normalize high-level features:
+# Validate the shapes:
 
-# In[ ]:
+# In[14]:
 
 
-hl_mean, hl_std = np.mean(hl, axis=0), np.std(hl, axis=0)
-hl = (hl - hl_mean) / hl_std
+print("Shape LL", ll.shape)
+print("Shape HL", hl.shape)
+print("Shape targets", targets.shape)
 
 
 # Split the data, use 90% for training and 10% for validation:
 
-# In[ ]:
+# In[15]:
 
 
 n_data = ll.shape[0]
 split = int(n_data * 0.9)
+print("Splitting data into", split, "training and", n_data-split, "validation events")
 
 ll_train, hl_train, ll_val, hl_val = ll[:split], hl[:split], ll[split:], hl[split:]
 y_train, y_val = targets[:split], targets[split:]
 
 
-# Calculate the class weights:
+# Calculate the class weights which we will later use in the fit procedure:
 
-# In[ ]:
+# In[16]:
 
 
 i_targets = np.argmax(targets, axis=1)
-class_weight = compute_class_weight('balanced', np.unique(i_targets), i_targets)
+class_weight = compute_class_weight('balanced', classes=np.unique(i_targets), y=i_targets)
 class_weight = dict(enumerate(class_weight))
+print("Class weight:", class_weight)
 
 
 # ## Build the model
 # Now we set up the model with `tf.keras`.
 # First some general network parameters:
 
-# In[ ]:
+# In[17]:
 
 
 n_classes = 2
@@ -248,8 +231,8 @@ dropout = 0.1
 # In[ ]:
 
 
-ll_inputs = tf.keras.Input(shape=(5, 4))
-hl_inputs = tf.keras.Input(shape=(1,))
+ll_inputs = tf.keras.Input(shape=(5, 4), name="LL")
+hl_inputs = tf.keras.Input(shape=(1,), name="HL")
 
 
 # Initialize the Lorentz Boost Network and use it as first layer of the network:
@@ -267,6 +250,7 @@ lbn_features = lbn_layer(ll_inputs)
 
 
 normalized_lbn_features = tf.keras.layers.BatchNormalization()(lbn_features)
+normalized_hl_vars = Normal(ref=hl, axis=0)(hl_inputs)
 
 
 # Concatenate the normalized features from the LBN and the high level features:
@@ -274,7 +258,7 @@ normalized_lbn_features = tf.keras.layers.BatchNormalization()(lbn_features)
 # In[ ]:
 
 
-x = tf.keras.layers.concatenate([normalized_lbn_features, hl_inputs])
+x = tf.keras.layers.concatenate([normalized_lbn_features, normalized_hl_vars])
 
 
 # Add the feed forward network constisting of some dense layers with dropout between the layers:
@@ -283,8 +267,10 @@ x = tf.keras.layers.concatenate([normalized_lbn_features, hl_inputs])
 
 
 x = tf.keras.layers.Dense(n_nodes, activation=activation)(x)
+x = tf.keras.layers.BatchNormalization()(x)
 x = tf.keras.layers.Dropout(dropout)(x)
 x = tf.keras.layers.Dense(n_nodes, activation=activation)(x)
+x = tf.keras.layers.BatchNormalization()(x)
 x = tf.keras.layers.Dropout(dropout)(x)
 outputs = tf.keras.layers.Dense(n_classes)(x)
 
@@ -323,16 +309,45 @@ model.fit(
     batch_size=1024,
     class_weight=class_weight,
     validation_data=([ll_val, hl_val], y_val),
-    verbose=2,
 )
 
 
-# Plot the confusion matrix of the trained model:
+# And plot a simple confusion matrix for the final classificaiton:
+
+# In[ ]:
+
+
+def plot_confusion_matrix(cm, class_names):
+    figure = plt.figure(figsize=(4, 4))
+    plt.imshow(cm, interpolation='nearest', cmap="summer")
+    plt.title("Confusion matrix")
+    plt.colorbar()
+    tick_marks = np.arange(len(class_names))
+    plt.xticks(tick_marks, class_names, rotation=45)
+    plt.yticks(tick_marks, class_names)
+
+    cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(i, j, cm[i, j], horizontalalignment="center", size=18)
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig("hh_tt.pdf")
+    plt.show()
+
 
 # In[ ]:
 
 
 pred_val = model.predict([ll_val, hl_val], batch_size=1024)
-confusion_matrix = confusion_matrix(np.argmax(y_val, axis=1), np.argmax(pred_val, axis=1))
-plot_confusion_matrix(confusion_matrix, ["TT", "HH"])
+cm = confusion_matrix(np.argmax(y_val, axis=1), np.argmax(pred_val, axis=1))
+plot_confusion_matrix(cm, ["TT", "HH"])
+
+
+# In[ ]:
+
+
+
 
